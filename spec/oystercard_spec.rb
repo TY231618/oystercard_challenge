d7ed6b209ed32ccc4b9eae200b793bff520caa0b
Tony Young <ty_training@hotmail.co.uk>
2015-12-01 07:20:21 +0000

    commented out vaccuous tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,7 @@ subject(:oystercard) { described_class.new }
   end
 
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
+    #it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'allows funds to be added' do
       expect{ oystercard.top_up(1) }.to change { oystercard.balance }.by(1)
@@ -24,7 +24,7 @@ subject(:oystercard) { described_class.new }
   end
 
   describe '#deduct' do
-    it { is_expected.to respond_to(:deduct).with(1).argument }
+    #it { is_expected.to respond_to(:deduct).with(1).argument }
 
     it 'allows funds to be deducted' do
       #oystercard.top_up(1)
